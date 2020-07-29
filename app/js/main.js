a225726b7bd32d957a89eaba1abee18b1775b83d
Vladimir Rybalsky <alevoldon@gmail.com>
2020-07-29 16:46:39 +0300

    update files

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(".navbar-collapse a").on('click', function(){
+	$(".navbar-collapse").collapse('hide');
+});
+
+$(window).scroll(function(){
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+});
