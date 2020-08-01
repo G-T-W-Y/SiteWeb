2c51111bbd1a6ae9670ef43715cc56f66f7864c6
Vladimir Rybalsky <alevoldon@gmail.com>
2020-08-01 20:34:34 +0300

    add section courses

--- js/script.js
+++ js/script.js
@@ -1,23 +1,47 @@
-$(".navbar-collapse a").on('click', function(){
-	$(".navbar-collapse").collapse('hide');
+$(".navbar-collapse a").on('click', function() {
+    $(".navbar-collapse").collapse('hide');
 });
 
-$(window).scroll(function(){
-	if ($(".navbar").offset().top > 50) {
-		$(".navbar-fixed-top").addClass("top-nav-collapse");
-	} else {
-		$(".navbar-fixed-top").removeClass("top-nav-collapse");
-	}
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
 });
 
 // Carousel
 
 $('.home-slider').owlCarousel({
-      animateOut: 'fadeOut',
-      items:1,
-      loop:true,
-      dots:false,
-      autoplayHoverPause: false,
-      autoplay: true,
-      smartSpeed: 1000,
-})
+    animateOut: 'fadeOut',
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    nav: false
+});
+
+$('.owl-courses').owlCarousel({
+    animateOut: 'fadeOut',
+    loop: true,
+    dots: false,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    nav: true,
+    navText: [
+        '<i class="fa fa-angle-left"></i>',
+        '<i class="fa fa-angle-right"></i>'
+    ],
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        1000: {
+            items: 3,
+        }
+    }
+});
