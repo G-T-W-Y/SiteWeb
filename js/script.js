88b3535d8eb1442bb814229c59a9b0a8a1306677
Vladimir Rybalsky <alevoldon@gmail.com>
2020-08-03 17:40:15 +0300

    update script

--- js/script.js
+++ js/script.js
@@ -71,4 +71,12 @@ $('.owl-client').owlCarousel({
 
 // WOW JS
 
-new WOW().init();
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: true, // default
+    live: true // default
+})
+
+wow.init();
